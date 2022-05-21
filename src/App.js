69879645dc423bdabf63548c853a2b9ddb2ed496
mohamdallam <mohamedahmedallam1@gmail.com>
2022-05-21 22:58:04 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import "./App.css";
+import React from "react";
 import Home from "./components/Home";
+import "./App.css";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This Is The Search Page</h1>
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Home.css";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
       <h1>This is Home Page</h1>
 
-      <div className="home__header"></div>
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+        </div>
+
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/image">Images</Link>
+        </div>
+      </div>
       <div className="home__body"></div>
     </div>
   );
