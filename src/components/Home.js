f71386d24e9d3320ef33637e40d807cf6e7a0b48
mohamdallam <mohamedahmedallam1@gmail.com>
2022-05-21 18:27:30 +0200

    Init

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>This is Home Page</h1>
+
+      <div className="home__header"></div>
+      <div className="home__body"></div>
+    </div>
+  );
+};
+
+export default Home;
